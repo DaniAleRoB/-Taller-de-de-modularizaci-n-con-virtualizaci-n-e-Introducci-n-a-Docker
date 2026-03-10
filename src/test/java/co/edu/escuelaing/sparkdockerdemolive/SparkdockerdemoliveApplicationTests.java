dab421d879ba136d38383df8106d6125ef4a6614
unknown <daniel.rodriguez-b@labinfo.is.escuelaing.edu.co>
2026-03-10 18:11:32 -0500

    first step docker

---
+++ src/test/java/co/edu/escuelaing/sparkdockerdemolive/SparkdockerdemoliveApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.sparkdockerdemolive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkdockerdemoliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
